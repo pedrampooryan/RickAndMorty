4fff6a01805a698ae0091b5a11eb2e1411858774
Pedram Pooryan <pedrampooryan@gmail.com>
2022-10-07 01:07:41 +0330

    Enable ViewBinding and create characters fragment.

---
+++ app/src/main/java/com/example/rickandmorty/characters/CharactersFragment.kt
@@ -0,0 +1,35 @@
+package com.example.rickandmorty.characters
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.rickandmorty.databinding.FragmentCharactersBinding
+
+
+class CharactersFragment : Fragment() {
+
+    private var _binding: FragmentCharactersBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       _binding = FragmentCharactersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ExText.text = "hi"
+    }
+
+}
